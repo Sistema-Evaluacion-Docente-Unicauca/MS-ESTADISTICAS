994d7cd3d099ae0a379c877bec0556d2053c6083
jhayber17 <jhayberm@unicauca.edu.co>
2025-04-06 11:38:27 -0500

    Feature: creacion de proyecto

---
+++ src/main/java/co/edu/unicauca/estadistica/EstadisticaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.estadistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadisticaApplication.class, args);
+	}
+
+}
